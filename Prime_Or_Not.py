ea779d539043e1381a8a40aa6367a8d360112d55
Swastik Saha <65535891+Swastik-Saha@users.noreply.github.com>
2020-10-01 12:06:53 +0530

    Update Prime_Or_Not.py
    No need of math module and checking divisibility till num/2 is enough to prove that its prime and code will be more efficient.

--- Prime_Or_Not.py
+++ Prime_Or_Not.py
@@ -1,7 +1,7 @@
-import math
+
 
 def isprime(num):
-        for i in range(2,num):
+        for i in range(2,(num/2)+1):
                 if(num%i == 0):
                         return "Not Prime"
                 else:
